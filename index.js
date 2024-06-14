77163071c44ecf43defd761464e0a28cd49711f9
augustine-awuori <codewithaugustine@gmail.com>
2024-06-14 17:48:23 +0300

    Configure app keys

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = express();
 const logging = require("./startup/logging");
 // const routes = require("./startup/routes");
 const db = require("./startup/db");
-// const config = require("./startup/config");
-const prod = require("./startup/prod.js");
+const config = require("./startup/config.js");
+const prod = require("./startup/prod");
 
+config();
 logging();
 // routes(app);
 db();
-// config();
 prod(app);
 
 const port = process.env.PORT || 3000;
