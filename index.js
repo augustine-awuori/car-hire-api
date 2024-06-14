a2129567829a5c5c3f6d5c99f39b07e860f6bbd0
augustine-awuori <codewithaugustine@gmail.com>
2024-06-14 17:37:44 +0300

    log message when successful

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const winston = require("winston");
 const express = require("express");
 const app = express();
 
@@ -16,7 +15,7 @@ logging();
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
-  winston.info(`Server is listening on port ${port}`)
+  console.log(`Server is listening on port ${port}`)
 );
 
 module.exports = server;
