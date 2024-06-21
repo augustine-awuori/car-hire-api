517148724036326a7059b41e4c4d222b5f13ce02
augustine-awuori <codewithaugustine@gmail.com>
2024-06-21 13:24:26 +0300

    Update car with relevant ownership documents

--- routes/cars.js
+++ routes/cars.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
 const { Car, validateCar } = require("../models/car.js");
 const auth = require("../middlewares/auth");
@@ -31,6 +32,27 @@ router.get("/", async (_req, res) => {
   const cars = await Car.find({});
 
   res.send(cars);
+});
+
+router.patch("/carId:", auth, async (req, res) => {
+  const carId = req.params.carId;
+
+  if (!mongoose.isValidObjectId(carId))
+    return res.status(400).send({ error: "Invalid ID provided" });
+
+  let car = await Car.findById(carId);
+  if (!car)
+    return res
+      .status(404)
+      .send({ error: "The car with the given Id doesn't exist" });
+
+  if (req.user._id.toString() !== car.lessee.toString())
+    return res.status(403).send({ error: "This isn't your car" });
+
+  car = await Car.findByIdAndUpdate(carId, req.body, { new: true }).populate(
+    "lessee"
+  );
+  res.send(car);
 });
 
 module.exports = router;
