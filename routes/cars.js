187584a40c3a437811c8fe5d45cb490fd9bd31d0
augustine-awuori <codewithaugustine@gmail.com>
2024-07-01 12:52:53 +0300

    Revert "Bug fix: Retrieve populated cars"
    This reverts commit 543208a75541f3e98c6cbba9700715375f3ae446.

--- routes/cars.js
+++ routes/cars.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 const { Car, validateCar } = require("../models/car");
+const { findById, populateAndProject } = require('../services/cars');
 const auth = require("../middlewares/auth");
-const service = require('../services/car');
 const validator = require("../middlewares/validate");
 
 router.post("/", [auth, validator(validateCar)], async (req, res) => {
@@ -12,11 +12,11 @@ router.post("/", [auth, validator(validateCar)], async (req, res) => {
 
   await car.save();
 
-  res.send(await service.findById(car._id));
+  res.send(await findById(car._id));
 });
 
 router.get("/", async (_req, res) => {
-  const cars = await service.getAll();
+  const cars = await populateAndProject(await Car.find({}).sort('-_id'));
 
   res.send(cars);
 });
@@ -27,8 +27,8 @@ router.get("/:carId", async (req, res) => {
   if (!mongoose.isValidObjectId(carId))
     return res.status(400).send({ error: 'Invalid car id' });
 
-  const car = await service.findById(carId);
-  if (car) return res.send(car);
+  const car = await Car.findById(carId);
+  if (car) return res.send(await populateAndProject(car));
 
   res.status(404).send({ error: "The car with the given id doesn't exist" })
 });
@@ -39,7 +39,7 @@ router.patch("/:carId", auth, async (req, res) => {
   if (!mongoose.isValidObjectId(carId))
     return res.status(400).send({ error: "Invalid ID provided" });
 
-  let car = await service.findById(carId);
+  let car = await Car.findById(carId);
   if (!car)
     return res
       .status(404)
@@ -48,7 +48,8 @@ router.patch("/:carId", auth, async (req, res) => {
   if (req.user._id.toString() !== car.lessee.toString())
     return res.status(403).send({ error: "This isn't your car" });
 
-  res.send(await service.findByIdAndUpdate(carId, req.body, { new: true }));
+  car = await populateAndProject(await Car.findByIdAndUpdate(carId, req.body, { new: true }));
+  res.send(car);
 });
 
 module.exports = router;
