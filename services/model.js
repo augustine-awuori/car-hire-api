3a985d0fa3c22bd9a4facc404b1d09d55d93bc73
augustine-awuori <codewithaugustine@gmail.com>
2024-07-01 13:52:16 +0300

    Code refactor

--- services/model.js
+++ services/model.js
@@ -13,11 +13,10 @@ const getAll = async (filter = {}) =>
     await populateAndProject(Model.find(filter).sort("-_id"));
 
 const findByIdAndUpdate = async (id, update, options) => {
-    if (!mongoose.isValidObjectId(id)) return;
-
-    return await populateAndProject(
-        Model.findByIdAndUpdate(id, update, options)
-    );
+    if (mongoose.isValidObjectId(id))
+        return await populateAndProject(
+            Model.findByIdAndUpdate(id, update, options)
+        );
 };
 
 module.exports = { findById, findByIdAndUpdate, getAll, populateAndProject }
