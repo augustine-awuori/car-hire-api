0181bf585ea65f94b7d621fa6d7bad1bb1730d89
augustine-awuori <codewithaugustine@gmail.com>
2024-07-01 09:47:46 +0300

    Authorised admins to create car types

--- routes/types.js
+++ routes/types.js
@@ -3,18 +3,16 @@ const router = express.Router();
 
 const { Type, validate } = require("../models/type");
 const admin = require('../middlewares/admin');
+const auth = require('../middlewares/auth');
 const validatingWith = require('../middlewares/validate');
 
-router.post("/", [
-    // auth,
-    // admin,
-    validatingWith(validate)], async (req, res) => {
-        const type = new Type({ label: req.body.label });
+router.post("/", [auth, admin, validatingWith(validate)], async (req, res) => {
+    const type = new Type({ label: req.body.label });
 
-        await type.save();
+    await type.save();
 
-        res.send(type);
-    });
+    res.send(type);
+});
 
 router.get("/", async (_req, res) => {
     const types = await Type.find({}).sort("_id");
